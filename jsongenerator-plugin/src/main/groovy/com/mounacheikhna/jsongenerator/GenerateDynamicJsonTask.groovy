edb1a7141ec9dcdde16a10d5608b47e345b06859
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-02-17 19:46:18 +0100

    create the right files and folder to generate.

--- jsongenerator-plugin/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateDynamicJsonTask.groovy
+++ jsongenerator-plugin/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateDynamicJsonTask.groovy
@@ -1,6 +1,7 @@
 package com.mounacheikhna.jsongenerator
 
 import groovy.json.JsonSlurper
+import org.apache.commons.collections.map.HashedMap
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -30,21 +31,22 @@ class GenerateDynamicJsonTask extends DefaultTask implements GenerateDynamicJson
     }
 
     void generateDynamicJsons(String locale, String jsonPropetiesPath, String propertiesPath, String imagesPropertiesFilePath) {
-        //TODO: create folder for generated files
-        File generatedFolder = new File("${project.projectDir}/generated").mkdirs()
-
+        File generatedFolder = new File("${project.projectDir.getPath()}/generated")
+        generatedFiles.mkdirs()
         def definitions = slurper.parse(new File("${getProject().projectDir.getPath()}/definitions.json"))
         println "definitions : $definitions"
 
         definitions.locales.each {
             println "local data : $it"
-            String generatedJsonFileName = it.generatedName.replace("{{locale}}", locale);
+            String generatedFileName = it.generatedName.replace("{{locale}}", locale);
+            File generatedFile = new File("${generatedFolder.getPath()}/$generatedFileName")
             File templateFile = new File("${it.template}.json")
 
+            fillPlaceholders(templateFile, generatedFile, new HashedMap<>());
         }
     }
 
-    def fillPlaceholders(File inputFile, File outputFile, Map<String, String> properties) {
+    static def fillPlaceholders(File inputFile, File outputFile, Map<String, String> properties) {
         def inputFileText = inputFile.getText('UTF-8')
         properties.each {
             key, value ->
