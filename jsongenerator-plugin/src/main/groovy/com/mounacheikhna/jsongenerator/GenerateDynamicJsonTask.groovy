273d6a974d287f38f8942404d14db2a0c603b408
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-02-17 19:33:45 +0100

    Add fill placeholders + parse json definitions file.

--- jsongenerator-plugin/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateDynamicJsonTask.groovy
+++ jsongenerator-plugin/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateDynamicJsonTask.groovy
@@ -25,24 +25,37 @@ class GenerateDynamicJsonTask extends DefaultTask implements GenerateDynamicJson
             throw new IllegalArgumentException("Please provide a correct path to properties file : $propertiesPath ")
         }
         slurper = new JsonSlurper()
+
         generateDynamicJsons(locale, jsonPropertiesFilePath, propertiesPath, imagesPropertiesFilePath)
     }
 
     void generateDynamicJsons(String locale, String jsonPropetiesPath, String propertiesPath, String imagesPropertiesFilePath) {
-        //for now since in properties file we only have -> But maybe a json config instead would be better
-        /*Properties properties = ParseUtils.parseProperties(jsonPropetiesPath)
-          properties.findAll { k, v -> k.contains("template") }.each {
-            generatedJsonName, templateName ->
-                println " "
-        }*/
+        //TODO: create folder for generated files
+        File generatedFolder = new File("${project.projectDir}/generated").mkdirs()
 
         def definitions = slurper.parse(new File("${getProject().projectDir.getPath()}/definitions.json"))
         println "definitions : $definitions"
 
         definitions.locales.each {
             println "local data : $it"
+            String generatedJsonFileName = it.generatedName.replace("{{locale}}", locale);
+            File templateFile = new File("${it.template}.json")
+
         }
     }
+
+    def fillPlaceholders(File inputFile, File outputFile, Map<String, String> properties) {
+        def inputFileText = inputFile.getText('UTF-8')
+        properties.each {
+            key, value ->
+                if(inputFileText.contains("{{$key}}")) {
+                    inputFileText = inputFileText.replaceAll("{{$key}}", value.toString())
+                }
+        }
+        outputFile.write(inputFileText, 'UTF-8')
+        outputFile
+    }
+
 
     @Override
     void locale(String locale) {
