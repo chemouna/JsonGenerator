82f72f582799525fe08f0e5f60fd13754810d890
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-06-23 11:43:19 +0200

    Fix multiple issues and support multiple configs.

--- jsongenerator/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateJsonSpec.groovy
+++ jsongenerator/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateJsonSpec.groovy
@@ -3,11 +3,11 @@ package com.mounacheikhna.jsongenerator
 public interface GenerateJsonSpec {
 
     public void locale(String locale)
-    public void firstPassConfig(String firstPassConfigPath, String startKeyDelimiter,
-                         String endKeyDelimiter, boolean escaped)
+    public void addFirstPassConfig(String firstPassConfigPath, String startKeyDelimiter,
+                                   String endKeyDelimiter, boolean escaped)
     public void secondPassConfig(String secondPassConfigPath, String startKeyDelimiter,
-                          String endKeyDelimiter, boolean escaped)
+                                 String endKeyDelimiter, boolean escaped)
     public void productFlavor(String productFlavor)
-    public void jsonsMappings(Map<String, String> mapping)
+    public void outputFileNamesMappings(Map<String, String> mapping)
 
 }
--- jsongenerator/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateJsonTask.groovy
+++ jsongenerator/src/main/groovy/com/mounacheikhna/jsongenerator/GenerateJsonTask.groovy
@@ -3,6 +3,8 @@ package com.mounacheikhna.jsongenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
+import java.util.regex.Matcher
+
 
 /**
  * Task to generate Json files per locale.
@@ -10,22 +12,23 @@ import org.gradle.api.tasks.TaskAction
 public class GenerateJsonTask extends DefaultTask implements GenerateJsonSpec {
 
     private String locale
-
-    private PassConfig firstPassConfig
+    private List<PassConfig> firstPassConfigs = new ArrayList<>()
     private PassConfig secondPassConfig
     private List<File> generatedFiles
     private String productFlavor
-    private Map<String, String> jsonsMappings = [:]
+    private Map<String, String> outputFileNamesMappings = [:]
 
     @TaskAction
     public void performTask() {
         generatedFiles = new ArrayList<>()
-        if (firstPassConfig == null) {
+        if (firstPassConfigs == null || firstPassConfigs.size() == 0) {
             throw new IllegalArgumentException("Please provide a config file.")
         }
-        if (firstPassConfig.validate()) {
-            generateJsonForLocale()
+
+        firstPassConfigs.each {
+            if (!it.isValid()) return
         }
+        generateJsonForLocale()
     }
 
     @Override
@@ -33,16 +36,16 @@ public class GenerateJsonTask extends DefaultTask implements GenerateJsonSpec {
         this.locale = locale
     }
 
-    @Override
-    public void firstPassConfig(String firstPassConfigPath, String startKeyDelimiter, String endKeyDelimiter,
-                         boolean escaped) {
-        firstPassConfig =
-                new PassConfig(firstPassConfigPath, startKeyDelimiter, endKeyDelimiter, escaped)
+    public void addFirstPassConfig(String firstPassConfigPath, String startKeyDelimiter, String endKeyDelimiter,
+                                   boolean escaped) {
+        firstPassConfigs.add(
+                new PassConfig(firstPassConfigPath, startKeyDelimiter, endKeyDelimiter, escaped))
+
     }
 
     @Override
     public void secondPassConfig(String secondPassConfigPath, String startKeyDelimiter,
-                          String endKeyDelimiter, boolean escaped) {
+                                 String endKeyDelimiter, boolean escaped) {
         secondPassConfig =
                 new PassConfig(secondPassConfigPath, startKeyDelimiter, endKeyDelimiter, escaped)
     }
@@ -52,61 +55,50 @@ public class GenerateJsonTask extends DefaultTask implements GenerateJsonSpec {
         this.productFlavor = productFlavor
     }
 
-    @Override
-    public void jsonsMappings(Map<String, String> mapping) {
-        this.jsonsMappings.putAll(mapping)
+    public void outputFileNamesMappings(Map<String, String> mapping) {
+        this.outputFileNamesMappings.putAll(mapping)
     }
 
     private void generateJsonForLocale() {
-        firstPassConfig.extractValues()
-        secondPassConfig.extractValues()
-        JsonConfig jsonConfig = new JsonConfig(locale, productFlavor, firstPassConfig, secondPassConfig)
-        jsonConfig.createJsons()
+        firstPassConfigs.each {
+            it.extractValues()
+        }
+        if(secondPassConfig != null) {
+            secondPassConfig.extractValues()
+        }
+
+        List<PassConfig> configs = firstPassConfigs
+        String baseFolder = "${project.projectDir}/src/$productFlavor/res/raw"
+        outputFileNamesMappings.each {
+            placeholderFileName, newFileName ->
+                println " placeholderFileName : $placeholderFileName and newFileName : $newFileName"
+                File placeholderFile = new File("${baseFolder}/$placeholderFileName")
+                File file = new File("${baseFolder}/$newFileName".toLowerCase())
+                Map<String, String> values = new HashMap<>()
+                configs.each {
+                    values.putAll(it.extractedValues)
+                }
+                println " values to replace $values"
+                fillPlaceholders(placeholderFile, file, values, configs)
+        }
     }
 
-    public class JsonConfig {
-        private String locale
-        private String baseFolder
-        PassConfig firstPassConfig
-        PassConfig secondPassConfig
-
-        public JsonConfig(String locale, String productFlavor, PassConfig firstPassConfig,
-                   PassConfig secondPassConfig) {
-            this.locale = locale
-            this.firstPassConfig = firstPassConfig
-            this.secondPassConfig = secondPassConfig
-            baseFolder = "${project.projectDir}/src/$productFlavor/res/raw"
-        }
-
-        void createJsons() {
-            jsonsMappings.each {
-                placeholder, newFileName ->
-                    createJson(placeholder, newFileName.toLowerCase())
+    File fillPlaceholders(File inputFile, File outputFile, Map<String, String> values, List<PassConfig> configs) {
+        def inputFileText = inputFile.getText('UTF-8')
+        PassConfig config = configs.get(0)
+        values.each { key, value ->
+            if (config.escaped) {
+                inputFileText = inputFileText.replaceAll(
+                        "\\${config.startKeyDelimiter}$key\\${config.endKeyDelimiter}",
+                        value.toString())
+            } else {
+                inputFileText = inputFileText.replaceAll(
+                        "${config.startKeyDelimiter}$key${config.endKeyDelimiter}",
+                        Matcher.quoteReplacement(value.toString()))
             }
         }
-
-        File createJson(placeholderFileName, jsonFileName) {
-            def placeholderFile = new File("${baseFolder}/$placeholderFileName")
-            File file = new File("${baseFolder}/$jsonFileName")
-            File newFile = fillPlaceholders(placeholderFile, file, firstPassConfig)
-            return fillPlaceholders(newFile, file, secondPassConfig)
-        }
-
-        File fillPlaceholders(File inputFile, File outputFile, PassConfig passConfig) {
-            def inputFileText = inputFile.getText('UTF-8')
-            passConfig.extractedValues.each { key, value ->
-                if (passConfig.escaped) {
-                    inputFileText = inputFileText.replaceAll(
-                            "\\${passConfig.startKeyDelimiter}$key\\${passConfig.endKeyDelimiter}",
-                            value.toString())
-                } else {
-                    inputFileText = inputFileText.replaceAll(
-                            "${passConfig.startKeyDelimiter}$key${passConfig.endKeyDelimiter}", value.toString())
-                }
-            }
-            outputFile.write(inputFileText, 'UTF-8')
-            outputFile
-        }
+        outputFile.write(inputFileText, 'UTF-8')
+        outputFile
     }
 
     public class PassConfig {
@@ -130,12 +122,13 @@ public class GenerateJsonTask extends DefaultTask implements GenerateJsonSpec {
             }
         }
 
-        public boolean validate() {
-            if (filePath?.trim() && !new File(filePath).exists()) {
+        public boolean isValid() {
+            if (filePath?.trim() && !(new File(filePath).exists())) {
                 throw new IllegalArgumentException(
                         "Please provide a correct path to config file : $filePath ")
             }
             return true
         }
     }
+
 }
